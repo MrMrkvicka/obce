f185b087b1c4a9073b2e9708a2b6393810afc82c
MrMrkvicka <smid_matej@oauh.cz>
2025-02-05 14:58:40 +0100

    2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,33 +6,23 @@ use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
 use App\Models\Obec;
 use App\Models\Okres;
-
-
+use App\Models\Kraj;
 
 
 class Home extends BaseController
 {
 
-
-
     var $obec;
     var $okres;
-  
+    var $kraj;
+    
     public function __construct()
     {
         $this->obec = new Obec();
         $this->okres = new Okres();
-       
+        $this->kraj = new Kraj();
+        
     }
-
-
-
-
-
-
-
-
-
 
     public function index(): string
     {
@@ -43,18 +33,10 @@ class Home extends BaseController
     public function index1(): void
     {
         $tabulka = $this->obec->findAll();
-        $data["mista"] = $tabulka;
+        $data["okres"] = $tabulka;
         echo view("index1", $data);
     }
 
   
-
-
-
-
-
-
-
-
 
 }
--- app/Views/index1.php
+++ app/Views/index1.php
@@ -5,6 +5,58 @@
 <?php $this->section("content"); ?>
 
 
+<h1>ghj</h1>
+
+<div class="row">
+    <div class="col-12">
+
+
+
+    <?php
+
+$table = new \CodeIgniter\View\Table();
+
+$table->setHeading("kod","Název","kraj");
+
+
+foreach($okres as $row){
+    $table->addRow($row->kod,$row->nazev,$row->kraj);
+}
+
+$template = array(
+    'table_open'=> '<table class="table table-bordered">',
+    'thead_open'=> '<thead>',
+    'thead_close'=> '</thead>',
+    'heading_row_start'=> '<tr>',
+    'heading_row_end'=>' </tr>',
+    'heading_cell_start'=> '<th>',
+    'heading_cell_end' => '</th>',
+    'tbody_open' => '<tbody>',
+    'tbody_close' => '</tbody>',
+    'row_start' => '<tr>',
+    'row_end'  => '</tr>',
+    'cell_start' => '<td>',
+    'cell_end' => '</td>',
+    'row_alt_start' => '<tr>',
+    'row_alt_end' => '</tr>',
+    'cell_alt_start' => '<td>',
+    'cell_alt_end' => '</td>',
+    'table_close' => '</table>'
+    );
+    
+    $table->setTemplate($template);
+
+echo $table->generate();
+
+
+// var_dump($hraci);
+
+?>
+    </div>
+
+</div>
+
+
 
 
 
