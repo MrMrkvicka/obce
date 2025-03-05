775aa3b1158d4771f9252a5056f1a26e7d857cb8
MrMrkvicka <smid_matej@oauh.cz>
2025-03-05 14:26:26 +0100

    skoro hotovo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -16,55 +16,57 @@ class Home extends BaseController
     var $obec;
     var $okres;
     var $kraj;
-    
+    var $data = [];
+
+
     public function __construct()
     {
         $this->obec = new Obec();
         $this->okres = new Okres();
         $this->kraj = new Kraj();
         
+
+
+        $this->data["okres"] = $this->okres->where("kraj",141)->findAll();
+        //  $tabulka = $this->okres->where("kod",$kodOkresu)->findAll();
+
+        
+        
+          
+  
     }
 
 
     
     public function index(): string
     {
-        return view('welcome_message');
+
+        return view('index1', $this->data);
     }
 
 
-    public function index1()
-    {
-        $tabulka = $this->okres->where("kraj",141)->findAll();
-      //  $tabulka = $this->okres->where("kod",$kodOkresu)->findAll();
-        $data["okres"] = $tabulka;
 
 
-        echo view("index1", $data);
-
-        
-    }
-
-
-    public function okres($kod, $perPage)
+    public function okres($kod, $perPage = 20)
     {
 
-        $data["obec"] = $this->obec->select(select:"obec.nazev, count(*) as pocet ")-> 
-        join("cast_obce", "obec-kod = cast_obce.obec","inner")->
-        join("ulice", "ulice cast_obce = cast_obce.kod","inner")->join
+        $this->data["obec"] = $this->obec->select(select:"obec.nazev, count(*) as pocet ")-> 
+        join("cast_obce", "obec.kod = cast_obce.obec","inner")->
+        join("ulice", "ulice.cast_obce = cast_obce.kod","inner")->join
         ("adresni_misto", "adresni_misto.ulice = ulice.kod", "inner")->join
         ("okres", "okres.kod = obec.okres")->
         where("okres.kod", $kod)->
         groupBy("obec.kod")->
         orderBy("pocet", "desc")->
         paginate($perPage);
-        $data["perPage"] = $perPage;
-        $data["kod"] = $kod;
-        $pager = $this->obec->pager;
-        $data["pager"] = $pager;
-        echo view ("okres", $data);
-
         
+        $this->data["perPage"] = $perPage;
+        $this->data["kod"] = $kod;
+        $pager = $this->obec->pager;
+        $this->data["pager"] = $pager;
+        echo view ("okres", $this ->data);
+
+
     }
 
 
